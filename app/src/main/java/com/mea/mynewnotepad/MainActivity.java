68022d8ad9ccbce9de969d4da46432c1f2a4e4f3
Evgeny-mea <70842210+Evgeny-mea@users.noreply.github.com>
2021-03-23 22:55:23 +0700

    23.03.21 - add toolBar

--- app/src/main/java/com/mea/mynewnotepad/MainActivity.java
+++ app/src/main/java/com/mea/mynewnotepad/MainActivity.java
@@ -1,14 +1,36 @@
 package com.mea.mynewnotepad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_layout);
-    }
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.button_menu, menu);
+        return true;
+    }
+
+
+
+
+}
+
